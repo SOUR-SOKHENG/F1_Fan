d64f7cb67020022fadee5cc2d8b5aa5efdbe1417
SOUR-SOKHENG <Brogamer75x@gmail.com>
2026-06-04 13:14:19 +0700

    fix: add deployment base path

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import react from '@vitejs/react-swc' // or '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/

8c901c89af0e014acb46b40652e4ea4f01780b93
SOUR-SOKHENG <Brogamer75x@gmail.com>
2026-06-04 12:35:43 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/F1_Fan/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()]  }),

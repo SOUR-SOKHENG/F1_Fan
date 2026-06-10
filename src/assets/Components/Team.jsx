ccf3325c2b2052c0ed22d608c2c87645b375dad3
SOUR-SOKHENG <Brogamer75x@gmail.com>
2026-06-10 10:54:21 +0700

    some respon

--- src/assets/Components/Team.jsx
+++ src/assets/Components/Team.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
+import './Css/Team.css'
 
 const Team = () => {
+  
+
+  
   return (
     <div className='Big-box'>
       <article className='container-fluid text-center'>
         <h1 className='text-center text-danger'>Grid LineUp</h1>
         <p className=' w-full mx-auto'>Meet the constructors, team masterminds, and elete driver Parings fighting wheel-to-wheel on the world stage.</p>
       </article>
-
+      <section className=' '>
+        <div className=''></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </section>
     </div>
   )
 }
